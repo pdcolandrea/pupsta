f62c3df3ef1210ebe49f7a4dbda23d0fc619761d
Paul <colandreapaul@gmail.com>
2023-02-02 20:48:27 -0500

    Initial Commit

---
+++ Pupsta/PupstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PupstaApp.swift
+//  Pupsta
+//
+//  Created by Paul  Colandrea on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct PupstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
